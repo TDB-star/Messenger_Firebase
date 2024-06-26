7e9f544460214e151709c516a3a3552603a9c35a
TDB-star <tatiana.blache@gmail.com>
2024-06-26 14:19:28 +0200

    added reset password view model + user model

--- app/src/main/java/com/example/messengerfirebase/ForgotPasswordActivity.java
+++ app/src/main/java/com/example/messengerfirebase/ForgotPasswordActivity.java
@@ -1,6 +1,8 @@
 package com.example.messengerfirebase;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.content.Context;
 import android.content.Intent;
@@ -8,12 +10,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class ForgotPasswordActivity extends AppCompatActivity {
 
     private static final String EXTRA_EMAIL = "email";
     private EditText editTextEmail;
     private Button resetButton;
+    private ResetPasswordViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +26,8 @@ public class ForgotPasswordActivity extends AppCompatActivity {
 
         editTextEmail = findViewById(R.id.editTextEmail);
         resetButton = findViewById(R.id.buttonReset);
+        viewModel = new ViewModelProvider(this).get(ResetPasswordViewModel.class);
+        observeViewModel();
 
         String email = getIntent().getStringExtra(EXTRA_EMAIL);
 
@@ -34,6 +40,34 @@ public class ForgotPasswordActivity extends AppCompatActivity {
             public void onClick(View v) {
                 String email = editTextEmail.getText().toString().trim();
                 // todo reset password
+                viewModel.resetPassword(email);
+            }
+        });
+    }
+
+    private void observeViewModel() {
+        viewModel.getError().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(String errorMessage) {
+                if (errorMessage != null) {
+                    Toast.makeText(
+                                    ForgotPasswordActivity.this,
+                                    errorMessage,
+                                    Toast.LENGTH_SHORT)
+                            .show();
+                }
+            }
+        });
+        viewModel.isSuccess().observe(this, new Observer<Boolean>() {
+            @Override
+            public void onChanged(Boolean success) {
+                if (success) {
+                    Toast.makeText(
+                                    ForgotPasswordActivity.this,
+                                    R.string.reset_password_link_send,
+                                    Toast.LENGTH_SHORT)
+                            .show();
+                }
             }
         });
     }
--- app/src/main/java/com/example/messengerfirebase/RegistrationActivity.java
+++ app/src/main/java/com/example/messengerfirebase/RegistrationActivity.java
@@ -1,6 +1,8 @@
 package com.example.messengerfirebase;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.content.Context;
 import android.content.Intent;
@@ -9,10 +11,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseUser;
 
 public class RegistrationActivity extends AppCompatActivity {
 
-    private TextView textViewRegistration;
     private EditText editTextEmail;
     private EditText editTextPassword;
     private EditText editTextName;
@@ -20,21 +24,55 @@ public class RegistrationActivity extends AppCompatActivity {
     private EditText editTextAge;
     private Button buttonSignUp;
 
+    private RegistrationViewModel viewModel;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
         initViews();
 
+        viewModel = new ViewModelProvider(this).get(RegistrationViewModel.class);
+        observeViewModel();
+
         buttonSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               String email = getText(editTextEmail);
-               String password = getText(editTextPassword);
-               String name = getText(editTextName);
-               String lastName = getText(editTextLastName);
-               int age = Integer.parseInt(getText(editTextAge));
-               // todo sign up
+                String email = getText(editTextEmail);
+                String password = getText(editTextPassword);
+                String name = getText(editTextName);
+                String lastName = getText(editTextLastName);
+                int age = Integer.parseInt(getText(editTextAge));
+                // todo sign up
+                viewModel.signUp(email, password, name, lastName, age);
+            }
+        });
+    }
+
+    private void observeViewModel() {
+        viewModel.getUser().observe(this, new Observer<FirebaseUser>() {
+            @Override
+            public void onChanged(FirebaseUser firebaseUser) {
+                // todo intent to usersActivity screen
+                if (firebaseUser != null) {
+                    Intent intent = UsersActivity.newIntent(RegistrationActivity.this);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        });
+
+        viewModel.getError().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(String errorMessage) {
+                // todo Toast
+                if (errorMessage != null) {
+                    Toast.makeText(
+                            RegistrationActivity.this,
+                            errorMessage,
+                            Toast.LENGTH_SHORT)
+                            .show();
+                }
             }
         });
     }
@@ -44,7 +82,6 @@ public class RegistrationActivity extends AppCompatActivity {
     }
 
     private void initViews() {
-        textViewRegistration = findViewById(R.id.textViewRegistration);
         editTextEmail = findViewById(R.id.editTextEmail);
         editTextPassword = findViewById(R.id.editTextPassword);
         editTextName = findViewById(R.id.editTextName);
